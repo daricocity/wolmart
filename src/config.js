8a76c6a10362a206b0401123e7d71844dcd3ae22
daricocity <daricocity@gmail.com>
2023-01-26 04:27:51 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ const config = {
     defaultPath: '/',
     borderRadius: 12,
     fontFamily: `'Poppins', sans-serif`,
-    proxy: 'https://3oov49.deta.dev/api',
-    // proxy: 'https://wolmart-api.herokuapp.com/api',
+    proxy: 'https://wolmart-api.herokuapp.com/api',
+    // proxy: 'https://wolmart-api.deta.dev/api',
 };
 
 export default config;
